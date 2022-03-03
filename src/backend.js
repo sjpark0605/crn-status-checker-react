e059158004df6cb43a0e22c3fc166921975bb478
Se Jin Park <zcabsjp@ucl.ac.uk>
2022-03-03 15:19:45 +0900

    Use HTTPS

--- src/backend.js
+++ src/backend.js
@@ -26,7 +26,7 @@ function parseRows(rows) {
 
 function constructURL() {    
     let key = "TYL7RY2Rgdb1YnwnJoF40OPohnDgToDu3waCfXeuUsJwtWX0m9H%2Fp9qbmvOrL6trE4fAmUHr%2BbI50eraQ3cdpg%3D%3D"
-    let url = "http://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey="
+    let url = "https://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey="
 
     return url + key
 }
